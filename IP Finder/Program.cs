9b47aff0596669847f06337afdb2d3e47e0710e9
Cole Timlin <95893912+AstroLeapStudios@users.noreply.github.com>
2022-02-20 12:38:19 -0500

    Add files via upload

---
+++ IP Finder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;  
+using System.Collections.Generic;  
+using System.Linq;  
+using System.Text;  
+using System.Net;
+   
+   
+namespace IpProto  
+{  
+    class Program  
+    {  
+        static void Main(string[] args)  
+        {    
+            String strHostName = string.Empty;
+             strHostName = Dns.GetHostName();
+              Console.WriteLine("Local Machine's Host Name: " + strHostName);
+               IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+                IPAddress[] addr = ipEntry.AddressList; for (int i = 0; i < addr.Length; i++) 
+                {
+                     Console.WriteLine("IP Address {0}: {1} ", i, addr[i].ToString()); 
+                } 
+            Console.ReadKey();
+        }
+    }  
+}    
